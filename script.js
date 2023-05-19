3fef9f88ae9023bfbadcd2a10edfdc22aca73a69
Babajide Ibiayo <babajideibiayo@yahoo.com>
2023-05-19 12:30:59 +0100

    practiced JS array looping options

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 
  const fruits = ["orange", "banana", "mango", "watermelon"];
  fruits.unshift('carrot')
- console.log(fruits)
+ // console.log(fruits)
 
- 
+// List contents of the fruits array to console
+// fruits.map(fruit => console.log(fruit));
+fruits.forEach(fruit => console.log(fruit));
+
+
+/* for(let i=0; i < fruits.length; i++) {
+    console.log(fruits[i])
+} */
+
+/* for (fruit in fruits)
+    console.log(fruits[fruit]) */
